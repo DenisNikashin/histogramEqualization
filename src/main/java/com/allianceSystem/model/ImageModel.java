7b9983e1e2736f2f11e2a6088be46af03208eeca
Samoilenko Mariia <marija.samojlenko@gmail.com>
2018-01-27 15:40:47 +0200

    add mvn support +reading th file

---
+++ src/main/java/com/allianceSystem/model/ImageModel.java
@@ -0,0 +1,11 @@
+package com.allianceSystem.model;
+
+import java.awt.*;
+
+public class ImageModel {
+    private final Image image;
+
+    public ImageModel(Image image) {
+        this.image = image;
+    }
+}
